373c594e741d0302596abc84e7df2a138b58455a
Arnaud LAURENT <thunderace@free.fr>
2018-01-16 17:55:26 +0100

    copy firmware and spiffs in compiledbin

--- extra_script.py
+++ extra_script.py
@@ -4,7 +4,7 @@ import gzip
 import os
 #
 # Dump build environment (for debug)
-# print env.Dump()
+#print env.Dump()
 #
 
 #
@@ -21,4 +21,16 @@ def before_buildfs(source, target, env):
 		with gzip.open('./data/required.js.gz', 'wb') as f_out:
 			shutil.copyfileobj(f_in, f_out)
 
+def after_buildfs(source, target, env):
+	shutil.copy(spiffs_source, 'compiledbin/latestspiffs.bin')
+
+def after_build(source, target, env):
+	shutil.copy(firmware_source, 'compiledbin/latest.bin')
+
+
 env.AddPreAction('$BUILD_DIR/spiffs.bin', before_buildfs);
+env.AddPostAction('$BUILD_DIR/spiffs.bin', after_buildfs);
+env.AddPostAction("buildprog", after_build)
+
+spiffs_source = os.path.join(env.subst("$BUILD_DIR"), "spiffs.bin")
+firmware_source = os.path.join(env.subst("$BUILD_DIR"), "firmware.bin")
