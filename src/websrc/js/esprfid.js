ec5089f3c5ffd749791cd765fbf888f588a55649
Ömer Şiar Baysal <osiarbaysal@gmail.com>
2018-03-04 11:59:33 +0300

    Touch detect on mobile, swipe to open/close sidebar

--- src/websrc/js/esprfid.js
+++ src/websrc/js/esprfid.js
@@ -13,7 +13,7 @@ var config = {
         "ssid": "",
         "wmode": "",
         "pswd": "",
-        "offtime": ""
+        "offtime": "0"
     },
     "hardware": {
         "readerType": "1",
@@ -925,3 +925,44 @@ FooTable.MyFiltering = FooTable.Filtering.extend({
         }
     }
 });
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;                                                        
+
+function handleTouchStart(evt) {                                         
+    xDown = evt.touches[0].clientX;                                      
+    yDown = evt.touches[0].clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            $('#dismiss').click();
+        } else {
+$('#sidebarCollapse').click();
+            /* right swipe */
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+        } else { 
+            /* down swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
