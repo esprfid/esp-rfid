f7c5f02af6c491c76d3d0d4a7c322815838710dc
frenchie71 <marc.ahlgrim@web.de>
2019-09-02 14:32:00 +0200

    decipher magic numbers

---
+++ src/magicnumbers.h
@@ -0,0 +1,14 @@
+// reader types
+
+#define READER_MFRC522 0
+#define READER_WIEGAND 1
+#define READER_PN532 2
+#define READER_RDM6300 3
+#define READER_MFRC522_RDM6300 4
+#define READER_WIEGAND_RDM6300 5
+#define READER_PN532_RDM6300 6
+
+// timing constants
+
+#define COOLDOWN_MILIS 2000
+#define KEYBOARD_TIMEOUT_MILIS 10000
